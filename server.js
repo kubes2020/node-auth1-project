94b8073d5bd5f5ec674a802daf29a7e1e96bdce7
Brian Kubes <briankubes@gmail.com>
2020-11-09 13:16:13 -0800

    login is working

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet')
 const cors = require('cors')
 const UsersRouter = require('./users/users-router.js')
 const Users = require('./users/users-model.js')
+const session = require('express-session')
 
 
 const server = express()
@@ -13,6 +14,18 @@ server.use(helmet())
 server.use(morgan('dev'))
 server.use(express.json())
 server.use(cors())
+server.use(session({  
+    name: 'monkey',
+    secret: 'this is secret', 
+    cookie: {
+        maxAge: 1000 * 20, 
+        secure: false,
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false,
+
+}))
 
 server.post('/auth/register', async (req, res) => {
     try {
@@ -26,6 +39,19 @@ server.post('/auth/register', async (req, res) => {
         res.status(500).json({ message: err.message })
     }
 })
+
+server.post('/auth/login', async (req, res) => {
+    try {
+        const [user] = await Users.findBy({ username: req.body.username})
+        if (user && bcrypt.compareSync(req.body.password, user.password)) {
+            req.session.user = user
+            res.json({ message: `welcome back ${user.username}`})
+        }
+    } catch (err) {
+        res.status(500).json({message: err.message })
+    }
+})
+
 
 server.use('/api/users', UsersRouter)
 
